15a9c63df99c37e08917f5d6cd09bb559167ce89
leentasan <dellafebialfian@gmail.com>
2026-04-20 14:28:32 +0700

    feat: add axios instance and auth service

---
+++ src/frontend/lib/axios.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+import { useAuthStore } from "@/store/auth-store";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true
+});
+
+// Attach token otomatis di setiap request
+api.interceptors.request.use((config) => {
+    const token = useAuthStore.getState().token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+// Handle response error global
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            // Token expired/invalid, hapus cookie dan redirect ke login
+            document.cookie = "token=; max-age=0; path=/";
+            window.location.href = "/demo/auth/login";
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
